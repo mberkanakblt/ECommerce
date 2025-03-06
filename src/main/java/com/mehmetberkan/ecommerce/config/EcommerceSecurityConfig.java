781dac86184b968ae09644cb5f2524501393d4e7
mberkanakblt <berkans.akbuluts@gmail.com>
2025-03-06 13:23:06 +0300

    EcommerceSecurityConfig'e yorum satirlari eklendi.

--- src/main/java/com/mehmetberkan/ecommerce/config/EcommerceSecurityConfig.java
+++ src/main/java/com/mehmetberkan/ecommerce/config/EcommerceSecurityConfig.java
@@ -26,6 +26,9 @@ public class EcommerceSecurityConfig {
             req
                     .requestMatchers("/swagger-ui/**","v3/api-docs/**","/dev/v1/kullanici/**") // belirli bir URL adresine erişimi yönet
                     .permitAll() // yukarıdaki adrese ve adreslere izin ver.
+                    /**
+                     * Aşağıdakiler rollere göre izin verme şuanda roller kapalidir.!!
+                     */
 //                    .requestMatchers("/dev/v1/kategori/**").hasAuthority("KATEGORI_ADMIN")
 //                    .requestMatchers("/dev/v1/urun/**").hasAuthority("URUN_ADMIN")
                     // Yukarıdak, Oturum açanın yetki kimliği USER, ADMİN (VS)... tipindeyse erişime izin ver
